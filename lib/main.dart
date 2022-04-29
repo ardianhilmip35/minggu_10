350c052e258132b284d581d9f76dcc8d4c7a9406
ARDIAN HILMI PRAMULINTANG <74774957+ardianhilmip35@users.noreply.github.com>
2022-04-29 20:43:41 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:minggu_10/get_data.dart';
+import 'package:minggu_10/get_detail_screen.dart';
+
+void main() {
+  runApp(
+    MyApp()
+    );
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Get API',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: GetDataScreen(),
+    );
+  }
+
+}
